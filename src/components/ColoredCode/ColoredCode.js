cbd0b556993d39653036d385e2090b24110ebebf
yoann g <yoann.3d@gmail.com>
2021-10-02 04:45:28 +0200

    Basic project setup

---
+++ src/components/ColoredCode/ColoredCode.js
@@ -0,0 +1,5 @@
+function ColoredCode() {
+    return <div>Colored Code</div>
+}
+
+export default ColoredCode;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ColoredCode from './components/ColoredCode/ColoredCode';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ColoredCode />
   </React.StrictMode>,
   document.getElementById('root')
 );
