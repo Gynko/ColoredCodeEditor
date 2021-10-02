e22b2458d830eaf3de0c0ac649221b0330162d92
yoann g <yoann.3d@gmail.com>
2021-10-02 06:50:52 +0200

    Figuring out the algorithm - Research

--- src/components/InputEditor/InputEditor.js
+++ src/components/InputEditor/InputEditor.js
@@ -1,6 +1,21 @@
 import "./InputEditor.styles.css";
 
+//TODO:
+//Having an array of string elements that we want to be styled specifically.
+//Having a function that, on every keystroke, will check if there is in the input text a string matching an element in the array. Regex.
+//From there, when matching, figuring out how to somehow style the text displayed in the input text.
+
 function InputEditor() {
+  var listElements = [
+    "Primitive Type",
+    "Object",
+    "Null",
+    "String",
+    "Number",
+    "Symbol",
+    "Undefined",
+  ];
+
   return <input className="input-codeeditor" placeholder="code" />;
 }
 
