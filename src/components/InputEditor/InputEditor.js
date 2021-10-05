7c7b8ae40b9fdf45f54af19208ba5a4cfa6a4778
yoann g <yoann.3d@gmail.com>
2021-10-05 10:48:59 +0200

    input textarea aligned aligned with the div that will display the input transformed in html. Need utility react html parser and its dependcy

--- src/components/InputEditor/InputEditor.js
+++ src/components/InputEditor/InputEditor.js
@@ -2,30 +2,68 @@ import { useState, useEffect, useRef } from "react";
 import "./InputEditor.styles.css";
 
 function InputEditor() {
-  var [inputCode, setInputCode] = useState("gekki");
+  var [inputCode, setInputCode] = useState();
   var [heightWidth, setHeightWidth] = useState([0, 0]);
+  var [codeHtmled, setCodeHtmled] = useState(
+    <code
+      style={{
+        width: heightWidth[0],
+        height: heightWidth[1],
+        position: "absolute",
+        fontFamily: "Operator Mono",
+        color: "brown",
+        top: "0",
+        left: "0",
+        pointerEvents: "none",
+        fontSize: "1em",
+        margin: "0",
+        padding: "0",
+      }}
+    >
+      {inputCode}
+    </code>
+  );
+
   var ref = useRef(null);
 
   useEffect(() => {
     setHeightWidth([ref.current.clientHeight, ref.current.clientWidth]);
   }, []);
 
-  var listElements = [
-    "Primitive Type",
-    "Object",
-    "Null",
-    "String",
-    "Number",
-    "Symbol",
-    "Undefined",
-  ];
-
   function onInput(event) {
     setInputCode(event.target.value);
     setHeightWidth([ref.current.clientWidth, ref.current.clientHeight]);
+    var addingBreaks = inputStringToHtml(event.target.value);
+    setCodeHtmled(
+      <code
+        style={{
+          width: heightWidth[0],
+          height: heightWidth[1],
+          position: "absolute",
+          fontFamily: "Operator Mono",
+          color: "#ff7070",
+          top: "0",
+          left: "0",
+          pointerEvents: "none",
+          fontSize: "1em",
+          margin: "0",
+          padding: "0",
+        }}
+      >
+        {addingBreaks}
+      </code>
+    );
   }
 
-  function matchingKeywords(text) {
+  function inputStringToHtml(inputString) {
+    var match = inputString.replace(/\n/g, "<br />");
+    console.log(match);
+    return match;
+  }
+
+  //var listElements = ["var", "function", "null", "undefined"];
+
+  /*   function matchingKeywords(text) {
     var array = [];
     if (text !== "") {
       for (let i = 0; i < listElements.length; i++) {
@@ -39,45 +77,24 @@ function InputEditor() {
       array = array.flat();
     }
     return array;
-  }
+  } */
 
   function resize() {
     setHeightWidth([ref.current.clientWidth, ref.current.clientHeight]);
-    console.log(heightWidth);
   }
 
   return (
-    <div>
-      <div className="container">
-        <textarea
-          className="input-codeeditor"
-          placeholder={inputCode}
-          type="text"
-          ref={ref}
-          onInput={onInput}
-          onMouseUp={resize}
-          onMouseMove={resize}
-        />
-        <code
-          style={{
-            width: heightWidth[0],
-            height: heightWidth[1],
-            position: "absolute",
-            fontFamily: "Operator Mono",
-            color: "brown",
-            top: "0",
-            left: "0",
-            pointerEvents: "none",
-          }}
-        >
-          {inputCode}
-        </code>
-      </div>
-      <p>
-        {heightWidth.map((coord) => (
-          <span className="coord">{coord}</span>
-        ))}
-      </p>
+    <div className="container-inputeditor">
+      <textarea
+        className="input-codeeditor"
+        placeholder={inputCode}
+        type="text"
+        ref={ref}
+        onInput={onInput}
+        onMouseUp={resize}
+        onMouseMove={resize}
+      />
+      {codeHtmled}
     </div>
   );
 }
