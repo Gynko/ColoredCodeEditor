2643be5f7b302e437b41151926ad96b97b42dc18
yoann g <yoann.3d@gmail.com>
2021-10-05 06:48:15 +0200

    orienting towards a strategy where an html element is on top of the text area - experimenting

--- src/components/InputEditor/InputEditor.js
+++ src/components/InputEditor/InputEditor.js
@@ -1,13 +1,15 @@
+import { useState, useEffect, useRef } from "react";
 import "./InputEditor.styles.css";
 
-//TODO:
-//Having an array of string elements that we want to be styled specifically.
-//Having a function that, on every keystroke, will check if there is in the input text a string matching an element in the array. Regex.
-//From there, when matching, figuring out how to somehow style the text displayed in the input text.
-//Problem: not sure i can style directly the input value on the fly?
-//Possibility1: Can I Cut in 2? => The input text where we type & An output html that is our visual feedback "on top of where we type" ?
-
 function InputEditor() {
+  var [inputCode, setInputCode] = useState("gekki");
+  var [heightWidth, setHeightWidth] = useState([0, 0]);
+  var ref = useRef(null);
+
+  useEffect(() => {
+    setHeightWidth([ref.current.clientHeight, ref.current.clientWidth]);
+  }, []);
+
   var listElements = [
     "Primitive Type",
     "Object",
@@ -18,43 +20,64 @@ function InputEditor() {
     "Undefined",
   ];
 
-  var text =
-    "Oh a Primitive Type is in my text. How Null ! I don't want to be Undefined by this feature.. Primitive Type ";
-
-  var textConverted = "";
-
-  function analyzeText(text) {
-    var array = [];
-    for (let i = 0; i < listElements.length; i++) {
-      var concat = `${listElements[i]}`;
-      var regex = new RegExp(concat, "g");
-      var result = [...text.matchAll(regex)];
-      if (Array.isArray(result) && result.length !== 0) {
-        array.push(result);
-      }
-    }
-    array = array.flat();
-    textConverted = convertTextToHtml(text, array[0]);
-    return `I found these ${array}`;
+  function onInput(event) {
+    setInputCode(event.target.value);
+    setHeightWidth([ref.current.clientWidth, ref.current.clientHeight]);
   }
 
-  function convertTextToHtml(text, element) {
-    //The array that is returned by analyzeText contains, for each element, an "index": a number of where it found the match.
-    //I need then to remove the string of the match and replace it with some styled html.
-    //To remove, i need start of the input and its end. If index start is 9 and match length is 16 characters, index end is 25.
-    //Then i add appropriate html at index start.
-    console.log("element info", element.index, element[0], element[0].length);
-    console.log("text infos", text[element.index], text[element.index + 1]);
-    var outputHtml = "nothing yet";
-    return outputHtml;
+  function matchingKeywords(text) {
+    var array = [];
+    if (text !== "") {
+      for (let i = 0; i < listElements.length; i++) {
+        var concat = `${listElements[i]}`;
+        var regex = new RegExp(concat, "g");
+        var result = [...text.matchAll(regex)];
+        if (result.length !== 0) {
+          array.push(result);
+        }
+      }
+      array = array.flat();
+    }
+    return array;
+  }
+
+  function resize() {
+    setHeightWidth([ref.current.clientWidth, ref.current.clientHeight]);
+    console.log(heightWidth);
   }
 
   return (
     <div>
-      <p>{text}</p>
-      <input className="input-codeeditor" placeholder="code" type="text" />
-      <p>{analyzeText(text)}</p>
-      <p>{textConverted}</p>
+      <div className="container">
+        <textarea
+          className="input-codeeditor"
+          placeholder={inputCode}
+          type="text"
+          ref={ref}
+          onInput={onInput}
+          onMouseUp={resize}
+          onMouseMove={resize}
+        />
+        <code
+          style={{
+            width: heightWidth[0],
+            height: heightWidth[1],
+            position: "absolute",
+            fontFamily: "Operator Mono",
+            color: "brown",
+            top: "0",
+            left: "0",
+            pointerEvents: "none",
+          }}
+        >
+          {inputCode}
+        </code>
+      </div>
+      <p>
+        {heightWidth.map((coord) => (
+          <span className="coord">{coord}</span>
+        ))}
+      </p>
     </div>
   );
 }
